9b61d367e1aef1221a2e8877dcd52cfe747d6571
Rafael <rafaelbastiansch@gmail.com>
2021-09-11 23:34:44 -0300

    - separate component  - separate global styles

---
+++ components/StockChart.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="my_chart" />
+</template>
+
+<script>
+import { plotyStockChart } from '~/utils/chart.util'
+
+export default {
+  name: 'StockChart',
+  data () {
+    return {
+      csv: null
+    }
+  },
+  async fetch() {
+    this.csv = await this.$content('stock')
+      .fetch()
+    this.plotyChart()
+  },
+  methods: {
+    plotyChart () {
+      const data = this.csv[0].body
+      plotyStockChart(data, 'my_chart')
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,21 @@
 <template>
-  <div class="text-center">
-    <h1 class="title">Data chart with D3.js</h1>
-    <div id="my_chart"></div>
+  <div class="indexPage">
+    <stock-chart />
   </div>
 </template>
 
 <script>
-import { plotyStockChart } from '~/utils/chart.util'
+import StockChart from '~/components/StockChart.vue'
 
 export default {
-  data () {
-    return {
-      csv: null
-    }
-  },
-  async fetch() {
-    this.csv = await this.$content('stock')
-      .fetch()
-    this.plotyChart()
-  },
-  methods: {
-    plotyChart () {
-      const data = this.csv[0].body
-      plotyStockChart(data, 'my_chart')
-    }
+  components: {
+    StockChart
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.title {
-  font-family: 'Rubik';
-  font-size: 25px;
-  color: #444444;
-}
-.text-center {
-  text-align: center;
+.indexPage {
+  padding: 1rem;
 }
 </style>
